e24730ed0a559c5db7ea903d9e2372aef682ee30
David Wood <david.p.wood@gmail.com>
2016-06-11 20:45:16 +0100

    Add ava typings from package for node 5.0

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,6 +2,7 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
+/// <reference path="modules/ava/index.d.ts" />
 /// <reference path="modules/mime/index.d.ts" />
 /// <reference path="modules/sinon/index.d.ts" />
 /// <reference path="modules/source-map-support/index.d.ts" />
