5fa4c81ea7180e3cb6f5704a1196ef6509a759ed
David Wood <david.p.wood@gmail.com>
2016-06-12 14:01:41 +0100

    Improve build test

--- src/lib/build.ts
+++ src/lib/build.ts
@@ -11,7 +11,7 @@ export default (robot: IRobot) => {
       accountName: Config.appveyor.account,
       projectSlug: projectSlug
     });
-    
+
     res.reply('One moment please...');
 
     robot.http('https://ci.appveyor.com/api/builds')
--- src/test/build.test.ts
+++ src/test/build.test.ts
@@ -4,9 +4,16 @@ import * as sinon from 'sinon';
 import { MockRobot, MockRobotBrain, MockResponse, MockScopedHttpClient, MockSlackAdapter } from './helpers/mocks';
 import { IHttpClientHandler } from 'hubot';
 import * as express from 'express';
+import { Config } from '../lib/config';
 import BuildScript from '../lib/build';
 
 test('finbot > starts a build', (t) => {
+  // arrange
+  Config.appveyor.token = '12345';
+  Config.appveyor.account = 'some account';
+
+  const projectSlug = 'a project slug';
+
   const robot = new MockRobot();
   const respondStub = sinon.stub(robot, 'respond');
 
@@ -20,7 +27,7 @@ test('finbot > starts a build', (t) => {
   const response = new MockResponse();
   const replyStub = sinon.stub(response, 'reply');
 
-  response.match = ['a project slug'];
+  response.match = [null, projectSlug];
   response.message = {
     room: 'a room',
     user: {
@@ -34,6 +41,7 @@ test('finbot > starts a build', (t) => {
   robot.adapter = slackAdapter;
 
   const httpClient = new MockScopedHttpClient();
+  const headerSpy = sinon.spy(httpClient, 'header');
   const httpStub = sinon.stub(robot, 'http');
 
   httpStub.returns(httpClient);
@@ -46,8 +54,15 @@ test('finbot > starts a build', (t) => {
 
   respondStub.callsArgWith(1, response);
 
+  // act
   BuildScript(robot);
 
+  // assert
   t.true(respondStub.calledWith(/start build (.*)/i, sinon.match.func));
   t.true(replyStub.calledWith('One moment please...'));
+  t.true(httpStub.calledWith('https://ci.appveyor.com/api/builds'));
+  t.true(headerSpy.calledWith('Authorization', `Bearer ${Config.appveyor.token}`));
+  t.true(headerSpy.calledWith('Content-Type', 'application/json'));
+  t.true(headerSpy.calledWith('Accept', 'application/json'));
+  t.true(postStub.calledWith(`{"accountName":"${Config.appveyor.account}","projectSlug":"${projectSlug}"}`));
 });
